371816427205566520268d39e2ea5d2d48dec60f
Snehal20799 <snehalchavan20799@gmail.com>
2023-10-26 15:30:27 +0530

    my second commit

--- Repository_ninth.py
+++ Repository_ninth.py
@@ -2,3 +2,9 @@ str="GIT is very useful and Versatile....It is used to store the python projects
 print(str[0:19]+str[42:66])
 print(type(str))
 print(len(str))
+
+
+str1="GIT is very important tool"
+print(str1)
+
+print(len(str1))
